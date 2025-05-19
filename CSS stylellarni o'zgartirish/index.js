512f0661170a494b9e49ff329b4d35163ce11c80
khamzaevasad <135143741+khamzaevasad@users.noreply.github.com>
2025-05-19 20:55:32 +0900

    Add files via upload

---
+++ CSS stylellarni o'zgartirish/index.js
@@ -0,0 +1,15 @@
+// const heading = document.querySelector('h1');
+// // heading.setAttribute('style', 'margin: 45px');
+// console.log(heading.style);
+// heading.style.margin = '50px';
+
+const btnClick = document.querySelector('.btn-click');
+console.log(btnClick.style);
+btnClick.style.padding = '15px';
+btnClick.style.margin = '20px';
+btnClick.style.width = '200px';
+btnClick.style.borderRadius = '5px';
+btnClick.style.backgroundColor = 'crimson';
+btnClick.style.border = 'none';
+btnClick.style.color = 'white';
+btnClick.style.cursor = 'pointer';
