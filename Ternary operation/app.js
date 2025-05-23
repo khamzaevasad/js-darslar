cebdb86db291d5107391560dd0069126bcd48ff7
khamzaevasad <135143741+khamzaevasad@users.noreply.github.com>
2025-05-23 23:33:37 +0900

    Add files via upload

---
+++ Ternary operation/app.js
@@ -0,0 +1,17 @@
+const age = 19;
+
+age < 18
+  ? console.log('siz voyaga yetmagansiz')
+  : console.log('siz voyaga yetgansiz');
+
+// if elsedan foydalanilgan xolat
+// if (age < 18) {
+//   console.log('siz voyaga yetmagansiz');
+// } else {
+//   console.log('Siz voyaga yetgansiz');
+// }
+
+// Armiya yoshi 18/28
+age >= 18 && age <= 28
+  ? console.log('Armiyaga ketish yoshidasiz')
+  : console.log('armiyaga yoshingiz mos emas');
